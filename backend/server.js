21664dcb6e5994285ceadc7084d9fadbb3741723
shirang16 <shirang16@gmail.com>
2023-07-10 22:15:54 +0300

    upload

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 const courseRoutes = require('./routes/course')
 const lecturerRoutes = require('./routes/lecturer')
 const userRoutes = require('./routes/user')
